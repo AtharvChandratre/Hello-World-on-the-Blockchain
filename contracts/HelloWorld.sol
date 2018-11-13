6e8d22e37757fe3cd453599b6b039edd53b9f61b
Atharv Shripad Chandratre <f20170009@goa.bits-pilani.ac.in>
2018-11-13 22:07:11 +0530

    Initial Commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.23;
+
+//contract
+contract HelloWorld {
+
+    //functions
+    
+    function hi() public pure returns (string) {//basic function to return "Hello World"
+    	var words="Hello World";
+        return words;
+    }
+}
